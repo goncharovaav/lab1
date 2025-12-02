bd4a7942a869d68660b1ec3780b36eeaf4daf0c1
Anastasia <goncharovaanastasia53@gmail.com>
2025-12-02 21:42:32 +0700

    Initial commit

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from .import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+    path('authors/', views.author_list, name='authors'),
+    path('author/<int:pk>/', views.author_detail, name='author-detail'),
 ]
 
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
